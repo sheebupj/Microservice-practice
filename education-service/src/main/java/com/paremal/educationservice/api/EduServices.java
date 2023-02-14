42de4fead7a0394daef55d33ce2585b212e491b1
sheebupj <sheebupj@gmail.com>
2023-02-14 16:13:24 +0530

    saving of student added

--- education-service/src/main/java/com/paremal/educationservice/api/EduServices.java
+++ education-service/src/main/java/com/paremal/educationservice/api/EduServices.java
@@ -2,13 +2,18 @@ package com.paremal.educationservice.api;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.paremal.educationservice.model.Students;
 import com.paremal.educationservice.repo.StudentRepo;
 import com.paremal.educationservice.repo.UserRepo;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -32,6 +37,12 @@ public class EduServices {
 	@GetMapping("/allstudents")
 	public List<Students> allStudents(){
 		return sudentRepo.findAll();
+	}
+	
+	@PostMapping("/addstudent")
+	public ResponseEntity<Students> addStudent(@RequestBody Students student) throws URISyntaxException{
+	  Students savedStudent=sudentRepo.save(student);
+	  return ResponseEntity.created(new URI(savedStudent.getId().toString())).body(savedStudent);
 	}
 
 }
